c18cfefbc0ba3d8f1e9e7b7a9afc2a5c90d85254
fredi1574 <fredi1574@gmail.com>
2024-01-23 22:25:57 +0200

    updated calculator

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ function clearDisplay() {
 }
 
 function appendToDisplay(value) {
+  if (display.innerText === "Error" || display.innerText === "NaN") {
+    display.innerText = "";
+  }
+  if (value === "^") {
+    value = "**";
+  }
   display.innerText += value;
   console.log(display.innerHTML);
 }
@@ -21,6 +27,34 @@ function calculate() {
   }
 }
 
+function squareRoot() {
+  display.innerText = Math.sqrt(display.innerText);
+}
+
 function negate() {
   display.innerText *= -1;
+}
+
+function powerTen() {
+  display.innerText = Math.pow(10, display.innerText);
+}
+
+function logarithm() {
+  display.innerText = Math.log(display.innerText);
+}
+
+function naturalLogarithm() {
+  display.innerText = Math.log10(display.innerText);
+}
+
+function sine() {
+  display.innerText = Math.sin(display.innerText);
+}
+
+function cosine() {
+  display.innerText = Math.cos(display.innerText);
+}
+
+function tangent() {
+  display.innerText = Math.tan(display.innerText);
 }
