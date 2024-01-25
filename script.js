0a67355f5061bcba3768a62167958d3afc214153
fredi1574 <fredi1574@gmail.com>
2024-01-25 21:58:29 +0200

    first commit

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 const display = document.querySelector("#input");
-let expression = "";
 
 function clearDisplay() {
   display.innerText = "";
 }
 
 function appendToDisplay(value) {
+  // If the display is "Error" or "NaN", clear it before appending the new value
   if (display.innerText === "Error" || display.innerText === "NaN") {
     display.innerText = "";
   }
-  if (value === "^") {
-    value = "**";
-  }
+
   display.innerText += value;
-  console.log(display.innerHTML);
 }
 
 function deleteLast() {
+  // Remove the last character from the display
   display.innerText = display.innerText.slice(0, -1);
 }
 
 function calculate() {
+  // Replace all special characters with their corresponding values
+  display.innerText = display.innerText.replaceAll("^", "**");
+  display.innerText = display.innerText.replaceAll("×", "*");
+  display.innerText = display.innerText.replaceAll("÷", "/");
+  display.innerText = display.innerText.replaceAll("π", Math.PI);
+
+  // Evaluate the expression and display the result
   display.innerText = eval(display.innerText);
   if (display.innerText === "NaN") {
     display.innerText = "Error";
@@ -32,19 +37,23 @@ function squareRoot() {
 }
 
 function negate() {
+  // Multiply the current value by -1
   display.innerText *= -1;
 }
 
 function powerTen() {
+  // Raise the current value to the power of 10
   display.innerText = Math.pow(10, display.innerText);
 }
 
 function logarithm() {
-  display.innerText = Math.log(display.innerText);
+  // Calculate the logarithm of the current value
+  display.innerText = Math.log10(display.innerText);
 }
 
 function naturalLogarithm() {
-  display.innerText = Math.log10(display.innerText);
+  // Calculate the natural logarithm of the current value
+  display.innerText = Math.log(display.innerText);
 }
 
 function sine() {
